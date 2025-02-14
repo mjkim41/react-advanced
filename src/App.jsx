56ca4123010dda6603ca75002bce619417996513
futuredeveloper41 <mj10283@naver.com>
2025-02-14 09:23:56 +0900

    Cart modal 템플릿

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import Header from "./components/layout/Header.jsx";
 import Meals from "./components/Meals/Meals.jsx";
+import Cart from "./components/Cart/Cart.jsx";
+import {useState} from "react";
 
 const App = () => {
 
+    // 장바구니 모달 여닫는 상태변수
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    // 모달을 열어주는 함수
+    const handleShowCart = () => {
+        setCartIsShown(true);
+    }
+
+    // 모달을 닫아주는 함수
+    const handleHideCart = () => setCartIsShown(false);
+
   return (
     <>
-        <Header />
+        {cartIsShown && <Cart onClose={handleHideCart} />}
+        <Header onShowCart={handleShowCart}/>
         <div id="main">
             <Meals />
         </div>
